0bfb0544872f26919212709a130f1924ec751201
Zsolt Kovács <k.zsolt@live.com>
2017-08-17 10:20:41 +0200

    Initial commit

---
+++ src/SharpFish/Engine.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpFish
+{
+    public class Engine
+    {
+        private static StreamWriter _input;
+        private readonly Process _process;
+        private readonly Dictionary<string, string> _defaultOptions;
+        public Action<string> Result;
+
+        private Player _player;
+
+        public string CurrentPlayer => _player == Player.White ? "w" : "b";
+
+        public Engine()
+        {
+            var processStartInfo = new ProcessStartInfo("stockfish.exe");
+            processStartInfo.UseShellExecute = false;
+
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.RedirectStandardInput = true;
+            processStartInfo.RedirectStandardOutput = true;
+
+            Result = (r) => {};
+
+            _process = new Process { StartInfo = processStartInfo };
+            _process.OutputDataReceived += (sender, e) => Result(e.Data);
+            _process.Start();
+            _input = _process.StandardInput;
+
+            _process.BeginOutputReadLine();
+            _process.BeginErrorReadLine();
+
+            _defaultOptions = new Dictionary<string, string>();
+            _defaultOptions.Add("Write Debug Log", "false");
+            _defaultOptions.Add("Contempt Factor", "0");
+            _defaultOptions.Add("Contempt", "0");
+            _defaultOptions.Add("Min Split Depth", "0");
+            _defaultOptions.Add("Threads", "4");
+            _defaultOptions.Add("Hash", "1024");
+            _defaultOptions.Add("MultiPV", "1");
+            _defaultOptions.Add("Skill Level", "20");
+            _defaultOptions.Add("Move Overhead", "30");
+            _defaultOptions.Add("Minimum Thinking Time", "20");
+            _defaultOptions.Add("Slow Mover", "80");
+            _defaultOptions.Add("UCI_Chess960", "false");
+        }
+
+        public Engine(Player currPlayer): this()
+        {
+            SetCurrentPlayer(currPlayer);
+        }
+
+        public void Init(Dictionary<string, string> options = null)
+        {
+            _input.WriteLine("uci");
+
+            foreach (var option in _defaultOptions)
+            {
+                SetOption(option.Key, option.Value);
+            }
+
+            if (options != null)
+            {
+                foreach (var option in options)
+                {
+                    SetOption(option.Key, option.Value);
+                }
+            }
+        }
+
+        //2kr4/1pp2ppp/p3b3/4p2P/4P3/1P2PK1P/2PrN1B1/2R5 b - - 0 1
+        public void NewGame()
+        {
+            SetFenPosition("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+            _input.WriteLine("ucinewgame");            
+        }
+
+        public void SetCurrentPlayer(Player p)
+        {
+            _player = p;
+        }
+
+        public void SetPosition(IEnumerable<string> move)
+        {
+            _input.WriteLine("position startpos moves {0}", string.Join(" ", move));
+        }
+
+        public void SetFenPosition(string fen)
+        {
+            SetFenPosition(fen, "");
+        }
+        public void SetFenPosition(string fen, string currentPlayer)
+        {
+            _input.WriteLine("position fen {0} {1}", fen, currentPlayer);
+        }
+
+        public void SetOption(string name, string value)
+        {
+            _input.WriteLine("setoption name {0} value {1}", name, value);
+        }
+
+        private void Go(int depth)
+        {
+            _input.WriteLine("go depth {0}", depth);
+        }
+
+        public void FindBest()
+        {
+            Go(20);
+        }
+    }
+
+    public enum Player
+    {
+        White,
+        Black
+    }
+}
---
+++ src/SharpFish/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace SharpFish
+{
+    public class Program
+    {
+        private static Stack<string> moves;
+
+        private static string chessId = "";
+        private static Player player;
+        private static bool robot;
+        private static Engine engine;
+        private static bool run;
+        private const string url = "http://plainchess.timwoelfle.de/";
+
+        public static void Main(string[] args)
+        {
+            engine = new Engine();
+            moves = new Stack<string>();
+            run = true;
+
+            WriteColor("Robot? (y|n): ");
+            var rp = Console.ReadKey();
+            robot = rp.Key == ConsoleKey.Y;
+            Console.WriteLine();
+
+            WriteColor("Choose color (w|b): ");
+            var color = Console.ReadKey();
+            player = color.Key == ConsoleKey.W ? Player.White : Player.Black;
+            Console.WriteLine();
+
+            WriteColor("Chess ID: ");
+            chessId = Console.ReadLine();
+
+            engine.Init();
+            engine.NewGame();
+            engine.Result = Result;
+
+
+            var t = Task.Run(async () =>
+            {
+                if (robot && player == Player.Black)
+                    await JoinGame();
+
+                engine.FindBest();
+                while (run)
+                {
+                    await Task.Delay(5000);
+                    await MainAsync();
+                }
+            });
+            t.Wait();
+        }
+
+        static async Task MainAsync()
+        {
+            if (!string.IsNullOrEmpty(chessId))
+            {
+                string resultContent = await WaitForMove();
+                var actualMove = ParseMove(resultContent);
+                if (actualMove != moves.FirstOrDefault())
+                {
+                    moves.Push(actualMove);
+                    engine.SetPosition(moves.Reverse());
+                    engine.FindBest();
+                }
+            }
+
+        }
+
+        private static async Task JoinGame()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                var content = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>("id", chessId) });
+                await client.PostAsync("/php/joinGame.php", content);
+            }
+        }
+
+        private static async Task<string> WaitForMove()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                var content = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>("id", chessId) });
+                var result = await client.PostAsync("/php/waitForMove.php", content);
+                return await result.Content.ReadAsStringAsync();
+            }
+        }
+
+        private static async Task MakeMove(string move)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                var content = new FormUrlEncodedContent(new[] {
+                    new KeyValuePair<string, string>("id", chessId),
+                    new KeyValuePair<string, string>("startColumn", CharToNumber(move[0])),
+                    new KeyValuePair<string, string>("startRow", move[1].ToString()),
+                    new KeyValuePair<string, string>("endColumn",  CharToNumber(move[2])),
+                    new KeyValuePair<string, string>("endRow", move[3].ToString()),
+                });
+                var result = await client.PostAsync("php/makeMove.php", content);
+            }
+        }
+
+
+        private static string ParseMove(string m)
+        {
+            if (m.Length > 4 && !m.Contains("join"))
+                return NumberToChar(m[0]) + m[1] + NumberToChar(m[3]) + m[4];
+            return null;
+        }
+
+
+        private static string NumberToChar(char n)
+        {
+            switch (n)
+            {
+                case '1': return "a";
+                case '2': return "b";
+                case '3': return "c";
+                case '4': return "d";
+                case '5': return "e";
+                case '6': return "f";
+                case '7': return "g";
+                case '8': return "h";
+            }
+
+            throw new ArgumentOutOfRangeException();
+        }
+
+        private static string CharToNumber(char n)
+        {
+            switch (n)
+            {
+                case 'a': return "1";
+                case 'b': return "2";
+                case 'c': return "3";
+                case 'd': return "4";
+                case 'e': return "5";
+                case 'f': return "6";
+                case 'g': return "7";
+                case 'h': return "8";
+            }
+
+            throw new ArgumentOutOfRangeException();
+        }
+
+
+        public static void WriteColor(string key)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write(key);
+            Console.ResetColor();
+        }
+
+        private static void Result(string s)
+        {
+            if (s.Contains("bestmove"))
+            {
+                Console.Clear();
+
+                var r = s.Split(' ');
+                WriteColor("Chess ID: ");
+                Console.WriteLine(chessId);
+
+                WriteColor("Last move: ");
+                if (moves.Count() == 0)
+                {
+                    Console.WriteLine("STARTED");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write($"{(moves.Count() % 2 == 0 ? "B" : "W")} ");
+                    Console.ResetColor();
+
+                    Console.WriteLine(moves.FirstOrDefault());
+                }
+
+                WriteColor("Best move: ");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write($"{(moves.Count() % 2 == 0 ? "W" : "B")} ");
+                Console.ResetColor();
+                Console.WriteLine(r[1]);
+
+                //checkmate
+                if (r[1].Contains("none"))
+                    run = false;
+
+
+                var isYourTurn = ((moves.Count() % 2 == 0 || !moves.Any()) && player == Player.White) || (moves.Count() % 2 != 0 && player == Player.Black);
+
+                if (robot && isYourTurn)
+                {
+                    MakeMove(r[1]).GetAwaiter().GetResult();
+                }
+            }
+        }
+    }
+}
